dd36b48205511889399389c0c30c0b35f0357186
Druxorey <druxorey@gmail.com>
2024-08-06 21:27:17 -0400

    feat: configurable search-engine

---
+++ scripts/get_search_engine.js
@@ -0,0 +1,33 @@
+function hideSettings() {document.getElementById("settings-content").style.display = 'none';}
+function showSettings() {document.getElementById("settings-content").style.display = 'flex';}
+
+function changeSearchEngine() {
+    var form = document.getElementById('search-form');
+    var input = document.getElementById('search-input');
+    var searchEngine = document.getElementById("search-engine-selector");
+
+    form.action = searchEngine.value;
+    input.placeholder = "Search with " + searchEngine.options[searchEngine.selectedIndex].text;
+
+    localStorage.setItem('selectedSearchEngine', searchEngine.value);
+}
+
+function loadSavedSearchEngine() {
+    var savedSearchEngine = localStorage.getItem('selectedSearchEngine');
+
+    if (savedSearchEngine) {
+        var searchEngine = document.getElementById('search-engine-selector');
+        searchEngine.value = savedSearchEngine;
+        changeSearchEngine();
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        hideSettings();
+    } else if (event.key === ' ') {
+        document.getElementById('search-input').focus();
+    }
+});
+
+loadSavedSearchEngine();
